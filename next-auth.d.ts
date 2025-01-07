03e23054ff706aa62029fa1f210c571a0e07c5e4
ziiizar <cesarfpna@gmail.com>
2025-01-06 23:20:10 -0500

    Update next-auth.d.ts

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -1,8 +1,8 @@
-import { userRole } from "@prisma/client";
+import { UserRole } from "@prisma/client";
 import NextAuth, { type DefaultSession } from "next-auth";
 
 export type ExtendedUser = DefaultSession["user"] & {
-  role: userRole;
+  role: UserRole;
 };
 
 declare module "next-auth" {
