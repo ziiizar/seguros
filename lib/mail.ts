dbc731750a19caedc2ef87ce145c8f6e157af737
ziiizar <cesarfpna@gmail.com>
2025-01-30 15:48:21 -0500

    really fixed email

--- lib/mail.ts
+++ lib/mail.ts
@@ -10,9 +10,9 @@ export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/email-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "toyospablo@gpfservices.com",
     // to: email,
-    to: "toyospablo@gpfservices.com",
+    to: email,
     subject: "Confirm your email",
     html: `<p>Click<a href="${confirmLink}"> here </a>to confirm your email </p>`,
   });
@@ -25,7 +25,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
   const resetLink = `http://localhost:3000/new-password?token=${token}`;
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
+    from: "toyospablo@gpfservices.com",
     to: email,
     subject: "Reset your password",
     html: `<p>Click<a href="${resetLink}"> here </a>to reset your password </p>`,
@@ -37,8 +37,8 @@ export const sendNewLeadEmail = async (lead: Lead) => {
  
 
   await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: 'cesarfpna@gmail.com',
+    from: "toyospablo@gpfservices.com",
+    to: ['pabloetoyos0526@gmail.com'],
     subject: "New Lead",
     html: `<ul>
     <li>Name: <h4>${lead.name} </h4> </li>
