61b9903929dfc38892f0453571ece1837a4fa3e1
ziiizar <cesarfpna@gmail.com>
2025-02-06 16:05:22 -0500

    trying to deploy

--- lib/db.ts
+++ lib/db.ts
@@ -1,17 +1,35 @@
-import { PrismaClient } from "@prisma/client";
-import { NODE_ENV, TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "./env.config";
-import { createClient } from "@libsql/client/web";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+// import { PrismaClient } from "@prisma/client";
+// import { NODE_ENV, TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "./env.config";
+// import { createClient } from "@libsql/client/web";
+// import { PrismaLibSQL } from "@prisma/adapter-libsql";
 
+// const prismaGlobal = global as typeof global & { prisma?: PrismaClient };
+
+// // export const db = prismaGlobal.prisma || new PrismaClient();
+// const libsql = createClient({
+//     url: TURSO_DATABASE_URL || "",
+//     authToken: TURSO_AUTH_TOKEN,
+//   });
+
+//   const adapter = new PrismaLibSQL(libsql);
+//   export const db = prismaGlobal.prisma || new PrismaClient({ adapter });
+
+// if (NODE_ENV !== "production") prismaGlobal.prisma = db;
+
+
+import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+import { NODE_ENV, TURSO_AUTH_TOKEN, TURSO_DATABASE_URL } from "./env.config";
+
+const libsql = createClient({
+  url: TURSO_DATABASE_URL!,
+  authToken: TURSO_AUTH_TOKEN, 
+});
+
+const adapter = new PrismaLibSQL(libsql);
 const prismaGlobal = global as typeof global & { prisma?: PrismaClient };
 
-// export const db = prismaGlobal.prisma || new PrismaClient();
-const libsql = createClient({
-    url: TURSO_DATABASE_URL || "",
-    authToken: TURSO_AUTH_TOKEN,
-  });
-
-  const adapter = new PrismaLibSQL(libsql);
-  export const db = prismaGlobal.prisma || new PrismaClient({ adapter });
+export const db = prismaGlobal.prisma || new PrismaClient({ adapter });
 
 if (NODE_ENV !== "production") prismaGlobal.prisma = db;
