a5f59aef3416ef75be2a1c99644aeba27c82e720
ziiizar <cesarfpna@gmail.com>
2025-02-14 11:58:57 -0500

    header fixed for admin on mobile

--- components/global/Header/Header.tsx
+++ components/global/Header/Header.tsx
@@ -4,6 +4,7 @@ import { auth } from "@/auth";
 import { Button } from "@/components/ui/Button";
 import HeaderLinks from "./HeaderLinks"; 
 import Logo from "@/Icons/Logo";
+import Link from "next/link";
 
 const Header = async ({ className }: { className?: string }) => {
   const session = await auth();
@@ -11,7 +12,8 @@ const Header = async ({ className }: { className?: string }) => {
   return (
     <header
       className={cn(
-        " flex items-center place-content-center py-8 px-4 md:px-20 h-[10dvh] sm:-ms-8", session?.user?.role === "ADMIN" && " justify-between",
+        "flex items-center place-content-center py-8 px-4 md:px-20 h-[10dvh] sm:-ms-8", 
+        session?.user?.role === "ADMIN" && "justify-between",
         className
       )}
     >
@@ -22,15 +24,25 @@ const Header = async ({ className }: { className?: string }) => {
         <HeaderLinks />
       </nav>
 
-      <div className="">
+      <div className="max-sm:relative">
         {session?.user.role && session?.user.role === "ADMIN" && (
-          <Button className="w-32">
-            <a href={routes.admin}>Admin</a>
-          </Button>
+          
+            <Link href={routes.admin}> <Button 
+            className="w-32 
+                      max-sm:fixed 
+                      max-sm:bottom-4 
+                      max-sm:right-4 
+                      max-sm:w-14 
+                      max-sm:h-14 
+                      max-sm:rounded-full 
+                      max-sm:z-50
+                      max-sm:p-0"
+          > <span className="max-sm:hidden block text-lg">Admin</span>
+            <span className="hidden max-sm:block text-lg">⚙️</span>
+          </Button></Link>
         )}
       </div>
     </header>
   );
 };
-
 export default Header;
